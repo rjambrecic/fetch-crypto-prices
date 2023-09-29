3ed7fcadaebd8e4d2effdd0a3dfecec4361a7c9c
“Robert <robert@airt.ai>
2023-09-29 17:08:51 +0200

    Crypto app generated

--- app/application.py
+++ app/application.py
@@ -1,56 +1,84 @@
 import asyncio
-import random
+import json
+from datetime import datetime
 
-from faststream import FastStream, Logger
+from pydantic import BaseModel, Field, NonNegativeFloat
+
+from faststream import ContextRepo, FastStream, Logger
 from faststream.kafka import KafkaBroker
-from pydantic import BaseModel, Field
-
-version = "0.1.0"
-title = "My FastStream service"
-description = "Description of my FastStream service"
-
-
-class Name(BaseModel):
-    name: str = Field(..., description="Name of the person")
-
-
-class Greeting(BaseModel):
-    greeting: str = Field(..., description="Greeting message")
-
+import requests
 
 broker = KafkaBroker("localhost:9092")
-app = FastStream(broker, title=title, version=version, description=description)
+app = FastStream(broker)
 
-to_greetings = broker.publisher(
-    "greetings",
-    description="Produces a message on greetings after receiving a meesage on names",
-)
+publisher = broker.publisher("new_crypto_price")
 
 
-@broker.subscriber("names", description="Consumes messages from names topic and produces messages to greetings topic")  # type: ignore
-async def on_names(msg: Name, logger: Logger) -> None:
-    result = f"hello {msg.name}"
-    logger.info(result)
-    greeting = Greeting(greeting=result)
-    await to_greetings.publish(greeting)
+class CryptoPrice(BaseModel):
+    price: NonNegativeFloat = Field(
+        ..., examples=[50000.0], description="Current price of the cryptocurrency in USD"
+    )
+    crypto_currency: str = Field(
+        ..., examples=["BTC"], description="Cryptocurrency symbol"
+    )
+
+
+@app.on_startup
+async def app_setup(context: ContextRepo):
+    context.set_global("app_is_running", True)
+
+
+@app.on_shutdown
+async def shutdown(context: ContextRepo):
+    context.set_global("app_is_running", False)
+
+    # Get all the running tasks and wait for them to finish
+    publish_tasks = context.get("publish_tasks")
+    await asyncio.gather(*publish_tasks)
+
+
+async def fetch_and_publish_crypto_price(
+    crypto_currency: str,
+    logger: Logger,
+    context: ContextRepo,
+    time_interval: int = 2,
+) -> None:
+    # Always use context: ContextRepo for storing app_is_running variable
+    while context.get("app_is_running"):
+        url = f"https://api.coinbase.com/v2/prices/{crypto_currency}-USD/spot"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # read json response
+            raw_data = json.loads(response.content)
+            price = raw_data["data"]["amount"]
+
+            new_crypto_price = CryptoPrice(
+                price=price, crypto_currency=crypto_currency
+            )
+            key = crypto_currency.encode("utf-8")
+            await publisher.publish(new_crypto_price, key=key)
+        else:
+            logger.warning(
+                f"Failed API request {url} at time {datetime.now()}"
+            )
+
+        await asyncio.sleep(time_interval)
 
 
 @app.after_startup
-async def publish_names() -> None:
-    async def _publish_names() -> None:
-        names = [
-            "Ana",
-            "Mario",
-            "Pedro",
-            "João",
-            "Gustavo",
-            "Joana",
-            "Mariana",
-            "Juliana",
-        ]
-        while True:
-            name = random.choice(names)  # nosec
-            await broker.publish(Name(name=name), topic="names")
-            await asyncio.sleep(2)
+async def publish_crypto_price(logger: Logger, context: ContextRepo):
+    logger.info("Starting publishing:")
 
-    asyncio.create_task(_publish_names())
+    crypto_currencies = ["BTC", "ETH"]
+    # start fetching and publishing crypto prices
+    publish_tasks = [
+        asyncio.create_task(
+            fetch_and_publish_crypto_price(
+                crypto_currency, logger, context
+            )
+        )
+        for crypto_currency in crypto_currencies
+    ]
+    # you need to save asyncio tasks so you can wait for them to finish at app shutdown (the function with @app.on_shutdown function)
+    context.set_global("publish_tasks", publish_tasks)
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,17 +1,19 @@
 import pytest
+
+from faststream import Context, TestApp
 from faststream.kafka import TestKafkaBroker
 
-from app.application import Greeting, Name, broker, on_names
+from app.application import CryptoPrice, app, broker
 
 
-@broker.subscriber("greetings")
-async def on_greetings(msg: Greeting) -> None:
+@broker.subscriber("new_crypto_price")
+async def on_new_crypto_price(msg: CryptoPrice, key: bytes = Context("message.raw_message.key")):
     pass
 
 
 @pytest.mark.asyncio
-async def test_on_names():
+async def test_message_was_published():
     async with TestKafkaBroker(broker):
-        await broker.publish(Name(name="John"), "names")
-        on_names.mock.assert_called_with(dict(Name(name="John")))
-        on_greetings.mock.assert_called_with(dict(Greeting(greeting="hello John")))
+        async with TestApp(app):
+            await on_new_crypto_price.wait_call(3)
+            on_new_crypto_price.mock.assert_called()
